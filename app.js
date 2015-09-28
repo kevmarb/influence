ca2f5128739c4a4197a3b52dfacc8b1f7c1a8380
kevin marburger <kevin.marburger26@gmail.com>
2015-09-28 19:38:11 +0200

    fonction mail et mail apparait dans le terminale

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var io = require ('socket.io');
-
 var express = require ('express');
 var app = express(); 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var http = require ('http').Server(app);
-
-app.get('/', function(req, res) {
-	
+app.get('/', function(req, res) {	
 	res.sendFile(__dirname + '/index.html');
 
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('choice', function(what){
+		console.log('chosen: ' + what);
+	});
+	socket.on('mail', function(mail){	
+		console.log('mail: ' + mail);
+	});
 });
 
 http.listen(3000, function () {
 	console.log('listening on *: 3000');
 });
+
+
+
+
+
+
